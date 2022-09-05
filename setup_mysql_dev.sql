9f3acaceb4287f2f2344e3db4d7ff68a6ca0fe59
kadzahk <juan427lol@gmail.com>
2022-09-05 17:20:26 -0500

    script that prepares a MySQL server [FIX]

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
 -- Script that prepares a MySQL server for the project
+-- Creates DB
 CREATE DATABASE IF NOT EXISTS hbnb_dev_dv;
-
+-- Creates user if not exists, identified with a password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
-
+-- Grant all privileges to user on DB
 GRANT ALL PRIVILEGES ON 'hbnb_dev_dv'.* TO 'hbnb_dev'@'localhost';
-
+-- Grant Select privileges to user on performance_schema DB
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
