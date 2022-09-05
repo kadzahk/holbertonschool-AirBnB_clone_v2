b770819b22e1f541b93b906a3ef5d79f47a25bb2
Canel420 <4521@holbertonstudents.com>
2022-09-05 17:47:02 -0500

    double quotes removed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if not exists, identified with a password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges to user on DB
-GRANT ALL PRIVILEGES ON 'hbnb_dev_dv'.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant Select privileges to user on performance_schema DB
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
