b3637396af5c5d08019adfac36408bbb2321193b
Canel420 <4521@holbertonstudents.com>
2022-09-05 17:45:38 -0500

    double quotes removed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,9 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates a database if doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Creates a user if doesn't exist with given credentials.
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
--- Grant created users USAGE privilege on everything.
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
--- Grant created users SELECT privilege on database performance_schema.
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Grant created users all privilege on database hbnb_test_db.
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Grant created users all privilege on database performance_schema.
