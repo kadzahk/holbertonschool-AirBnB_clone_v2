510e4aa3605aac46ad8862d94eebfa7e4f1612c3
Canel420 <4521@holbertonstudents.com>
2022-09-06 16:41:07 -0500

    missing beggining comments added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+
 from models.base_model import Base
 from models.user import User
 from models.state import State
