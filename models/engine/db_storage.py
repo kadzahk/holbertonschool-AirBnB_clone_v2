50d05fe0c8eca66d9673165811d5202434905c57
Canel420 <4521@holbertonstudents.com>
2022-09-06 12:39:00 -0500

    database storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,52 @@
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+user = getenv('HBNB_MYSQL_USER')
+password = getenv('HBNB_MYSQL_PWD')
+host = getenv('HBNB_MYSQL_HOST')
+database = getenv('HBNB_MYSQL_DB')
+env = getenv('HBNB_ENV')
+
+
+class DBStorage():
+    """ This class manages storage of hbnb models in MySQl database """
+    __classes = [User, State, City, Amenity, Place, Review]
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ Instantiation of class DBStorage """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user,  password, host, database),
+                                      pool_pre_ping=True)
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Method to return existing classes in database """
+        result = {}
+        if cls in self.__classes:
+            search = self.__session.query(cls)
+            for obj in search:
+                key = f'{obj.__class__.name}.{obj.id}'
+                result[key] = obj
+        elif cls is None:
+            for clas in self.__classes:
+                search = self.__session.query(cls)
+                for obj in search:
+                    key = f'{obj.__class__.name}.{obj.id}'
+                    result[key] = obj
+        return(result)
+
+    def new(self, obj):
+        """ Add the object to the current database session"""
+        self.__session.add(obj)
+
