517a093daccfe36743f869fb476c340ebda21edc
Canel420 <4521@holbertonstudents.com>
2022-09-07 14:26:44 -0500

    change user to respond to different storage

--- models/user.py
+++ models/user.py
@@ -2,26 +2,31 @@
 """This module defines a class User"""
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.review import Review
-from models.place import Place
 from sqlalchemy import Column, String
+from os import getenv
 
 
-
-class User(BaseModel, Base):
+class User(BaseModel, Base if (getenv('HBNB_TYPE_STORAGE') == 'db') else
+           object):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship(
-        "Place",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
-    reviews = relationship(
-        "Review",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship(
+            "Place",
+            backref='user',
+            cascade="all, delete",
+            passive_deletes=True)
+        reviews = relationship(
+            "Review",
+            backref='user',
+            cascade="all, delete",
+            passive_deletes=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
