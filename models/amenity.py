b7f971d64b85c7e84348181509305ddc0df19c27
Canel420 <4521@holbertonstudents.com>
2022-09-07 17:10:50 -0500

    change init method for kwargs and storage types for amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,15 +2,20 @@
 """ State Module for HBNB project """
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel, Base if (getenv('HBNB_TYPE_STORAGE') == 'db') else
+              object):
     """
     Amenity inherits from Basemodel and Base
     """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    places_amenities = relationship(
-        "Place",
-        secondary="place_amenity")
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        places_amenities = relationship(
+            "Place",
+            secondary="place_amenity")
+    else:
+        name = ""
