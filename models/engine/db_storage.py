cbb27dbe44926b29a0d6cef97fa76861c966b4a4
kadzahk <juan427lol@gmail.com>
2022-09-14 11:41:37 -0500

    update some part of our engine with some def close

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,3 +68,9 @@ class DBStorage():
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """
+        Call remove() method on the private session attribute
+        """
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,9 @@ class FileStorage():
         if key in self.__objects:
             del self.__objects[key]
             self.save()
+    def close(self):
+        """
+        call reload() method for deserializing
+        the JSON file to objects
+        """
+        self.reload()
